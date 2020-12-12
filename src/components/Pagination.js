0fbbc6bdb6d406baee2406ccd10f9b5c543cad2c
cr3xtony <sumonroytony@gmail.com>
2020-12-13 00:13:25 +0600

    completed pagination with react

---
+++ src/components/Pagination.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav>
+      <ui className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <a onClick={() => paginate(number)} href="!#" className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ui>
+    </nav>
+  );
+};
+
+export default Pagination;
